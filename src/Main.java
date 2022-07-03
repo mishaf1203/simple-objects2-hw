ab2b7a44b2a78fd513e09474855434db14905e99
mishaf1203 <sduhHDFGH345>
2022-07-03 19:57:02 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Author author1 = new Author("Name1", "Surname1");
-        Book book1 = new Book("Book")
+        Author author1 = new Author("Lev", "Tolstoy");
+        Book book1 = new Book("War and peace", 111, author1);
+
+        Author author2 = new Author("Alex", "Pushkin");
+        Book book2 = new Book("Winter Road", 1703, author2);
+
+
+        System.out.println("Год издания книги " + book1.getName()+ " - "+ book1.getYear()+ "; Автор книги "+ book1.getAuthor().getName()+ " "+ book1.getAuthor().getSurname());
+        System.out.println("Год издания книги " + book2.getName()+ " - "+ book2.getYear()+ "; Автор книги "+ book2.getAuthor().getName()+ " "+ book2.getAuthor().getSurname());
+
+
     }
 }
