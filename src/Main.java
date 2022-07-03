615fd21314dcabbd9dfd5f5719b3db501feaedb2
mishaf1203 <sduhHDFGH345>
2022-07-03 20:00:07 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
         System.out.println("Год издания книги " + book1.getName()+ " - "+ book1.getYear()+ "; Автор книги "+ book1.getAuthor().getName()+ " "+ book1.getAuthor().getSurname());
         System.out.println("Год издания книги " + book2.getName()+ " - "+ book2.getYear()+ "; Автор книги "+ book2.getAuthor().getName()+ " "+ book2.getAuthor().getSurname());
 
+        book1.setYear(1933);
+        System.out.println("Год издания книги " + book1.getName()+ " - "+ book1.getYear()+ "; Автор книги "+ book1.getAuthor().getName()+ " "+ book1.getAuthor().getSurname());
+
 
     }
 }
